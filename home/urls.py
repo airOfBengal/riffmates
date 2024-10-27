34665f410ad1fef22222c39b614cd3451bb08f9e
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2024-10-28 00:11:13 +0600

    add exercises

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views as home_views
 
 urlpatterns = [
-    path("credits/", views.credits, name='credits'),
+    path("credits/", home_views.credits, name='credits'),
+    path("about/", home_views.about, name='about'),
+    path("version/", home_views.version_info, name='version'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def credits(request):
     content = "Nicky\nRony"
 
     return HttpResponse(content, content_type="text/plain")
+
+def about(request):
+    content = "<h1>This is my third attempt to be a Django Developer in 2024</h1>"
+    return HttpResponse(content, content_type="text/html")
+
+
+def version_info(request):
+    content = {"version": 1.0}
+    return JsonResponse(content, content_type="application/json")
+
 
